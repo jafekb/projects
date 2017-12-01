f161458b56f1488fc193acd64c73ca168e74210b
Ben Jafek <jafek91@gmail.com>
2017-12-01 15:46:51 -0700

    color

--- Provo Corpus/get_color_proportions.py
+++ Provo Corpus/get_color_proportions.py
@@ -34,28 +34,33 @@ everything = {}
 count = 0
 
 for im_name in im_names:
-	one_row = {}
+	
 	image = plt.imread(im_name)
 
 	for ROW in tqdm(range(0, 600, 5)):
 		for COL in range(0, 800, 5):
+			#Pixel identification information
+			one_row = {}
 			count += 1
+
+			#Get the circle image.
 			color_localized, nonblack = circle(ROW, COL, image)
 
+			#Save the identification information
 			tot = color_localized.sum()
 			one_row['Total'] = tot
 			one_row['picture'] = os.path.basename(im_name)
 			one_row['X_center'] = ROW
 			one_row['Y_center'] = COL
 
-			for j in range(3):
-				copy = np.copy(color_localized)
-				channel = copy[:,:,j]
-				wrong = [k for k in range(3) if k!=j]
-				for n in wrong:
-					copy[:,:,n] = 0
-
-				one_row[colors[j]] = copy.sum()/tot
+			#RGB
+			R, G, B = color_localized[:,:,0], color_localized[:,:,1], color_localized[:,:,2]
+			red_proportion = R.sum()/tot
+			green_proportion = G.sum()/tot
+			blue_proportion = B.sum()/tot
+			
+			one_row['Red'] = red_proportion
+			one
 
 			everything[count] = one_row
 
@@ -64,8 +69,6 @@ for im_name in im_names:
 			dfout = dfout.transpose()
 			dfout.to_csv('Color_Proportions.csv', index=False)
 
-			if count ==5:
-				sys.exit(0)
 
 
 
